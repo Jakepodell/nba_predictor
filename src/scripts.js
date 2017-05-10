6aceae9f8f45c81155f878da54bdd5731c0dafb5
Jack Solon <jps386@cornell.edu>
2017-05-09 20:06:14 -0400

    Updated front end. Still needs to be connected to back end.

--- src/scripts.js
+++ src/scripts.js
@@ -1,8 +1,13 @@
+function train() {
+	var season = document.getElementById("train_season").value;
+	document.getElementById("forecast").disabled = false;
+}
+
 function readInput(){
-	var homeTeam = document.getElementById("home_team");
-	var awayTeam = document.getElementById("away_team");
+	var team1 = document.getElementById("team1");
+	var team2 = document.getElementById("team2");
  	// call perceptron to compute prediction
- 	var winner = homeTeam.value;
+ 	var winner = team1.value;
  	// display prediction
  	var div = document.getElementById('prediction');
 	div.innerHTML = 'Predicted winer: ' + winner; 
