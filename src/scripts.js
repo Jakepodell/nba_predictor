93d46a3b06bfa30f1882d20197f64a4850fc4ac9
Jack Solon <jps386@cornell.edu>
2017-05-09 21:18:54 -0400

    Changing scripts.js to connect to backend

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,45 @@
+// Populate selct object [select] with option values from array [options]
+function populateDropdown(select, options) {
+	for (var i = 0; i < options.length; i++) {
+		var opt = options[i];
+		var e = document.createElement("option");
+		e.textContent = opt;
+		e.value = opt;
+		select.appendChild(e);
+	}
+}
+
+$.get(
+	"/teams",
+	{},
+	function(data){
+		populateDropdown(document.getElementById("train_season"), data);
+		populateDropdown(document.getElementById("predict_season"), data);
+	}
+);
+// Populate dropdowns with data on window load
+window.onload = function (){
+
+	var options = Seasons.seasons;
+	var teams = Teams.teams;
+	console.log(options);
+	populateDropdown(document.getElementById("train_season"), options);
+	populateDropdown(document.getElementById("predict_season"), options);
+	populateDropdown(document.getElementById("team1"), teams);
+	populateDropdown(document.getElementById("team2"), teams);
+};
+
 function train() {
 	var season = document.getElementById("train_season").value;
+	//call Perceptron.train(season) and if successful enable the forecast button
 	document.getElementById("forecast").disabled = false;
 }
 
 function readInput(){
-	var team1 = document.getElementById("team1");
-	var team2 = document.getElementById("team2");
+	var team1 = document.getElementById("team1").value;
+	var team2 = document.getElementById("team2").value;
  	// call perceptron to compute prediction
- 	var winner = team1.value;
+ 	var winner = team1;
  	// display prediction
  	var div = document.getElementById('prediction');
 	div.innerHTML = 'Predicted winer: ' + winner; 
